626b15a2aea603bdfa2beb5fb6fc98edad8ec166
libin2 <libin610569@163.com>
2014-06-21 13:09:49 +0800

    Revert "netty_tomcat_session"
    This reverts commit 5a7bcb8a551c4e55ba1ca20e98f0378cf9094f39.

--- MySessionServer/src/com/lb/mysession/thread/BarrierHolder.java
+++
@@ -1,9 +0,0 @@
-package com.lb.mysession.thread;
-
-/*
- * every producer and consumer must implement this interface
- * Barrier is just a object lock for the synchronization between producer and consumer
- */
-public interface BarrierHolder {
-	Object getBarrier();
-}
